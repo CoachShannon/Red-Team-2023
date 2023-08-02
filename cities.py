ee0a8f6bc7087ec6b7c3e407af81f26ffb402c70
Shannon Coach <shannoncoach44@gmail.com>
2023-08-02 22:03:09 +0000

    Cities

---
+++ cities.py
@@ -0,0 +1,10 @@
+city_1 = "New York City"
+city_2 = "Los Angeles"
+city_3 = "Boston"
+city_4 = "Nashville"
+city_5 = "Charlotte"
+
+empty_list = []
+top_cities = ["New York City", "Los Angeles", "Boston", "Nashville", "Charlotte"]
+print(top_cities)
+print(top_cities[0])
