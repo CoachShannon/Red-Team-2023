bc719ddfa4a56257ef26eaba40abfe28d0b2747a
Shannon Coach <shannoncoach44@gmail.com>
2023-08-03 17:27:02 +0000

    del top cities

---
+++ week13_1.py
@@ -0,0 +1,17 @@
+top_cities = ["New York City", "Los Angeles", "Boston", "Nashville", "singapore", "Charlotte"]
+del top_cities[4]
+print(top_cities)
+
+print(top_cities[4])
+
+top_cities = ["New York City", "Los Angeles", "Boston", "Nashville", "singapore", "Charlotte"]
+del top_cities[3:]
+print(top_cities)
+
+top_cities = ["New York City", "Los Angeles", "Boston", "Nashville", "singapore", "Charlotte"]
+del top_cities[:]
+print(top_cities)
+
+top_cities = ["New York City", "Los Angeles", "Boston", "Nashville", "singapore", "Charlotte"]
+del top_cities
+print(top_cities)
