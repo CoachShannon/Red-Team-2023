afc47f99437b3291b2cdb9cc9d8d6acb5da59348
Shannon Coach <shannoncoach44@gmail.com>
2023-08-03 00:29:31 +0000

    decisions

---
+++ decisions.py
@@ -0,0 +1,18 @@
+import random
+
+number = random.randint(0,10)
+
+thresh = 5
+
+if number > thresh:
+    print("Big Number")
+  
+    
+if number > thresh:
+    print("Big Number")
+elif number < thresh:
+    print("Small Number")
+else: # elif number == 7:
+    print("number is", thresh)
+    
+print(number)
