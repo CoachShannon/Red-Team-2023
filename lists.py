1b9f7f105f58f5d7b23e568a96923adeef952a21
Shannon Coach <102562148+CoachShannon@users.noreply.github.com>
2023-08-05 12:05:03 -0400

    week 13 call (#9)

---
+++ lists.py
@@ -0,0 +1,24 @@
+var = []
+
+print(type(var))
+
+print(type("Hello"))
+
+var2 = [151, 152, 153, 154, "007"]
+
+print(var2)
+
+var2 = var2 + [721] # not inplace
+
+print(var2)
+
+var2.append(445) # inplace function
+
+print(var2)
+
+print(dir(var2))
+
+var2.reverse()
+print(var2)
+
+var3 + [0, 1, 2, 3, 4, 5]
