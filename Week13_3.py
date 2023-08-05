d244b1a79e94845e2d4071eb018c540057a419a3
Shannon Coach <shannoncoach44@gmail.com>
2023-08-05 23:43:45 +0000

    Week 13_3

--- Week13_3.py
+++ Week13_3.py
@@ -1,1 +1,24 @@
-
+#list checking presence
+for char in 'happy message':
+    print(char)
+    
+    
+    
+invited_guests = ['Kate', 'Joe', 'Quan', 'Bre', 'Bron', 'Nacyn']
+name = input('What is your name? ')
+if name in invited_guests:
+    print('Welcome! ')
+else:
+    print('Sorry your not invited. ')
+    
+    
+    
+#Reversal of the code above
+#Check linse (9,20) for refrence
+invited_guests = ['Kate', 'Joe', 'Quan', 'Bre', 'Bron', 'Nacyn']
+name = input('What is your name? ')
+if name not in invited_guests:
+    print('Sorry your not invited. ')
+else:
+    print('Welcome! ')
+    
