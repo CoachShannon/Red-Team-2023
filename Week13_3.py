d356f2a8688e77826ab1b5ba08e25201f18421b9
Shannon Coach <shannoncoach44@gmail.com>
2023-08-07 22:55:16 +0000

    update to branch

--- Week13_3.py
+++ Week13_3.py
@@ -22,3 +22,15 @@ if name not in invited_guests:
 else:
     print('Welcome! ')
     
+
+
+name_original = 'Jon Snow'
+name_new = name_original
+name_original ='Daenerys Targaryen'
+print(name_original, name_new)
+
+
+list_original = [1, 2, 3]
+list_new = list_original
+list_original[0] =  -5
+print('Original:', list_original, '\nNew:', list_new)
