85e3c60a30f2a02731943d39567c8298c4b10fb8
Shannon Coach <shannoncoach44@gmail.com>
2023-08-08 02:16:16 +0000

    intro to tuples

---
+++ tuples.py
@@ -0,0 +1,18 @@
+empty_tuple = ()
+
+one_el_tuple = (1,)
+
+one_el_tuple = 1,
+
+three_el_tuple = 1, 2, 3,
+
+print(three_el_tuple)
+
+#mutability is the biggest difference between lists and tuples
+#tuples are immutable (unchangeable), can be overwritten though
+
+user_data = ('John', 'American', 1964)
+user_data = ('Naire', 'Zambian', 1989)
+
+#cannot add 'append' or delete 'del'
+#strings are also immutable, similar to tuples
