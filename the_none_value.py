1c1677238c915c0f925203331669622c0725b928
Shannon Coach <shannoncoach44@gmail.com>
2023-08-10 02:45:41 +0000

    None

---
+++ the_none_value.py
@@ -0,0 +1,13 @@
+print_return = print('Hello world?')
+print(print_return)
+
+x = None
+
+if x:
+    print("None is True")
+elif x is False:
+    print ("None is False")
+else:
+    print("None is not True, or False, None is just None")
+    
+#None dosen't mean True or False, None is = to null (nothing)
